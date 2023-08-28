762a12452077303fa7bfea4d2ce45b0f3f3f0ecc
Maria <Maria@MARIANA>
2023-08-28 20:47:45 -0300

    Agregar archivos de proyecto.

---
+++ GnassoRecuperatorioPOO.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GnassoRecuperatorioPOO.Entidades;
+
+namespace GnassoRecuperatorioPOO.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar el largo del prisma:");
+            int largo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar la altura del prisma:");
+            int altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el ancho del prisma:");
+            int ancho = int.Parse(Console.ReadLine());
+
+            Prisma prisma = new Prisma(largo, altura, ancho);
+
+            Console.WriteLine($"{prisma.MostrarDatos()}");
+
+
+        }
+    }
+}
---
+++ GnassoRecuperatorioPOO.Entidades/Prisma.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+
+namespace GnassoRecuperatorioPOO.Entidades
+{
+    public class Prisma
+    {
+
+        private int _largo;
+        private int _altura;
+        private int _ancho;
+
+        public int  GetLargo()
+        {
+            return _largo;
+        }
+        public int  GetAltura()
+        {
+            return _altura;
+        } 
+        public int GetAncho()
+        {
+            return _ancho;
+        }
+
+
+        public Prisma(int largo, int altura, int ancho)
+        {
+
+            if(largo<= 0 || altura<=0 || ancho<= 0)
+            {
+                throw new ArgumentException("Valores no validos!!!");
+            }
+
+
+            _largo = largo;
+            _altura = altura;
+            _ancho = ancho;
+
+        }
+
+
+        public Prisma() :this(1,1,1) 
+        {
+            
+        }
+
+
+        private double GetAreaBase(int largo, int ancho)
+        {
+            return largo * ancho;
+        }
+
+        private double GetAreaAltura(int altura, int ancho)
+        {
+            return altura * ancho;
+        }
+
+        private double GetAreaAncho(int altura, int largo)
+        {
+            return altura * largo;
+        }
+
+        public double GetAreaPrisma( int largo, int ancho, int altura )
+        {
+            return 2 * (GetAreaBase(largo, ancho) + GetAreaAltura(altura, ancho) + GetAreaAncho(altura, largo));
+        }
+
+        public double GetVolumenPrisma(int largo,int ancho, int altura)
+        {
+            return largo * ancho * altura;
+
+        }
+
+        public double GetDiagonal(int largo, int ancho, int altura)
+        {
+            return Math.Sqrt( Math.Pow(largo, 2) +  Math.Pow(ancho, 2) + Math.Pow(altura, 2));
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendLine($"Prisma rectangular:");
+            datos.AppendLine($"Largo: {_largo}");
+            datos.AppendLine($"Ancho: {_ancho}");
+            datos.AppendLine($"ALtura: {_altura}");
+            datos.AppendLine($"Area de la base: {GetAreaBase(_largo, _ancho)}");
+            datos.AppendLine($"Area de la altura: {GetAreaAltura(_altura, _ancho)}");
+            datos.AppendLine(($"Area del ancho: {GetAreaAncho(_altura, _largo)}"));
+            datos.AppendLine($"Area del prisma: {GetAreaPrisma(_largo, _ancho, _altura)}");
+            datos.AppendLine($"Volumen del prisma: {GetVolumenPrisma(_largo, _ancho, _altura)}");
+            datos.AppendLine($"Disgonal: {GetDiagonal(_largo, _ancho, _altura)}");
+
+            return datos.ToString();
+        }
+    }
+}
